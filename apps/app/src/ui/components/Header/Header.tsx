e389815953aa1612aecb9107b6afbbae304a887b
= <=>
2022-04-13 07:38:00 -0300

    feat: add Header component

---
+++ apps/app/src/ui/components/Header/Header.tsx
@@ -0,0 +1,37 @@
+import { NativeStackHeaderProps } from '@react-navigation/native-stack'
+import { Button } from '@rneui/themed'
+import React from 'react'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+
+import { Container, Title, Action } from './HeaderStyles'
+
+export interface HeaderProps extends NativeStackHeaderProps {}
+
+const Header: React.FC<HeaderProps> = ({
+  navigation,
+  options,
+  route,
+  back,
+}) => {
+  const insets = useSafeAreaInsets()
+
+  return (
+    <Container style={{ paddingTop: insets.top, height: insets.top + 56 }}>
+      <Action>
+        {back && (
+          <Button
+            type="clear"
+            icon={{ name: 'arrow-back-ios', size: 20 }}
+            onPress={() => navigation.goBack()}
+          />
+        )}
+      </Action>
+
+      <Title>{options?.title}</Title>
+
+      <Action></Action>
+    </Container>
+  )
+}
+
+export default Header
---
+++ apps/app/src/ui/components/Header/HeaderStyles.ts
@@ -0,0 +1,17 @@
+import styled from '@emotion/native'
+import { Text } from '@rneui/themed'
+
+export const Container = styled.View`
+  padding-left: 8px;
+  padding-right: 8px;
+  flex-direction: row;
+  align-items: center;
+`
+export const Title = styled(Text)`
+  flex: 1;
+  text-align: center;
+`
+
+export const Action = styled.View`
+  width: 48px;
+`
