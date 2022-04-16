3b5f00cbef9b64fcf9cb4c4ce5fcdcd54f56ee35
Igor de Almeida <igoor1205@gmail.com>
2022-04-15 23:57:36 -0300

    fix: adjust controller error response

--- apps/api/src/application/controllers/Controller.ts
+++ apps/api/src/application/controllers/Controller.ts
@@ -22,23 +22,33 @@ export abstract class Controller {
     return { statusCode, body }
   }
 
+  private errorResponse(
+    statusCode: number,
+    error: DomainError,
+  ): IControllerResponse {
+    return this.response(statusCode, {
+      message: error.message,
+      ...error,
+    })
+  }
+
   public ok(data: any) {
     return this.response(200, data)
   }
 
   public badRequest(error: DomainError) {
-    return this.response(400, error)
+    return this.errorResponse(400, error)
   }
 
   public unauthorized(error: DomainError) {
-    return this.response(401, error)
+    return this.errorResponse(401, error)
   }
 
   public notFound(error: DomainError) {
-    return this.response(404, error)
+    return this.errorResponse(404, error)
   }
 
   public fail(error: Error) {
-    return this.response(500, new CommonError.UnexpectedError(error))
+    return this.errorResponse(500, new CommonError.UnexpectedError(error))
   }
 }
