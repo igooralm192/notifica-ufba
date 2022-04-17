83101f91d6566f72146ed11e4305c640dc643a7f
Igor de Almeida <igoor1205@gmail.com>
2022-04-17 12:30:01 -0300

    fix: remove vercel.json

--- apps/api/src/main/server.ts
+++ apps/api/src/main/server.ts
@@ -2,8 +2,6 @@ import { makeApp } from '@/main/config/app'
 import env from '@/main/config/env'
 import { makeTypeORMConnection } from '@/main/factories/helpers'
 
-console.log(process.env)
-
 makeTypeORMConnection()
   .connect()
   .then(() => {
