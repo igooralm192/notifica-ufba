ed1d71867368c5050073caa78125db52bb52e1b7
Igor de Almeida <igoor1205@gmail.com>
2022-04-26 19:35:57 -0300

    refactor(app): update LoginPresenter protocol

--- apps/app/src/ui/presenters/LoginPresenter.ts
+++ apps/app/src/ui/presenters/LoginPresenter.ts
@@ -1,8 +1,15 @@
-import { IFormPresenter } from '@/ui/types/form'
+import { Either } from '@notifica-ufba/utils'
+
+import { ILoginInput } from '@/domain/ports/inputs'
+import { ILoginOutput } from '@/domain/ports/outputs'
+import { ILoginErrors } from '@/domain/usecases'
 
 export interface ILoginFormValues {
   email: string
   password: string
 }
 
-export interface ILoginPresenter extends IFormPresenter<ILoginFormValues> {}
+export interface ILoginPresenter {
+  loading: boolean
+  login(input: ILoginInput): Promise<Either<ILoginErrors, ILoginOutput>>
+}
