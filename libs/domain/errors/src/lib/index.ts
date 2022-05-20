b5b1e8f7722d9ea435ce8beed2b2608b77e2acca
Igor de Almeida <igoor1205@gmail.com>
2022-05-20 14:41:29 -0300

    feat(errors): add SubscribeStudentToDisciplineGroup errors

--- libs/domain/errors/src/lib/index.ts
+++ libs/domain/errors/src/lib/index.ts
@@ -3,6 +3,9 @@ import * as CommonError from './common'
 import * as CreateStudentError from './create-student'
 import * as CreateUserError from './create-user'
 
+export { StudentDoesNotExistError } from './student/StudentDoesNotExistError'
+export { DisciplineGroupDoesNotExistError } from './discipline-group/DisciplineGroupDoesNotExistError'
+
 export {
   AuthenticateUserError,
   CommonError,
