9ffb6f8036b6e4193c1d180b8e58a6ff96f9921b
Igor de Almeida <igoor1205@gmail.com>
2022-05-22 15:00:44 -0300

    feat: add myapp DisciplineScreen

---
+++ apps/myapp/src/screens/discipline/DisciplinePresenter.tsx
@@ -0,0 +1,82 @@
+import { IDiscipline } from '@notifica-ufba/domain/entities'
+import { IReadDisciplinesUseCase } from '@notifica-ufba/domain/usecases'
+import { BaseError } from '@notifica-ufba/errors'
+
+import * as api from '@/api'
+import { useDispatch, useSelector } from '@/store'
+import { disciplinesAdded, selectAllDisciplines } from '@/store/disciplines'
+import { IPaginatedList } from '@/types/list'
+
+import React, { useContext, useState } from 'react'
+import Toast from 'react-native-toast-message'
+
+export interface DisciplinePresenterContextData {
+  loading: boolean
+  disciplines: IPaginatedList<IDiscipline>
+  getDisciplines(input: IReadDisciplinesUseCase.Input): Promise<void>
+}
+
+const DisciplinePresenterContext = React.createContext(
+  {} as DisciplinePresenterContextData,
+)
+
+export const DisciplinePresenter: React.FC = ({ children }) => {
+  const dispatch = useDispatch()
+
+  const [loading, setLoading] = useState(true)
+  const [disciplinesTotal, setDisciplinesTotal] = useState(0)
+
+  const disciplines = useSelector(selectAllDisciplines)
+
+  const getDisciplines = async ({
+    paginate,
+  }: IReadDisciplinesUseCase.Input) => {
+    setLoading(true)
+
+    try {
+      const disciplines = await api.discipline.getDisciplines({
+        paginate,
+      })
+
+      dispatch(disciplinesAdded(disciplines.results))
+
+      setDisciplinesTotal(disciplines.total)
+    } catch (err) {
+      const error = err as BaseError
+
+      Toast.show({
+        type: 'error',
+        text1: 'Erro ao retornar disciplinas.',
+        text2: error.message,
+      })
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <DisciplinePresenterContext.Provider
+      value={{
+        loading,
+        disciplines: {
+          results: disciplines,
+          total: disciplinesTotal,
+        },
+        getDisciplines,
+      }}
+    >
+      {children}
+    </DisciplinePresenterContext.Provider>
+  )
+}
+
+export const withDisciplinePresenter = (Component: React.FC) => {
+  return (props: any) => (
+    <DisciplinePresenter>
+      <Component {...props} />
+    </DisciplinePresenter>
+  )
+}
+
+export const useDisciplinePresenter = () =>
+  useContext(DisciplinePresenterContext)
---
+++ apps/myapp/src/screens/discipline/DisciplinesScreen.tsx
@@ -0,0 +1,49 @@
+import { IDiscipline } from '@notifica-ufba/domain/entities'
+
+import { Button } from '@/components'
+import { useAuth } from '@/contexts/auth'
+import { useStatusBar } from '@/contexts/status-bar'
+
+import React, { useEffect, useLayoutEffect } from 'react'
+import { FlatList } from 'react-native'
+
+import {
+  useDisciplinePresenter,
+  withDisciplinePresenter,
+} from './DisciplinePresenter'
+import { DisciplinesListItem } from './DisciplinesListItem'
+import { Container } from './DisciplinesStyles'
+
+export interface DisciplinesScreenProps {}
+
+const DisciplinesScreen: React.FC<DisciplinesScreenProps> = props => {
+  const presenter = useDisciplinePresenter()
+
+  const auth = useAuth()
+  const statusBar = useStatusBar()
+
+  const renderDisciplinesListItem = ({ item }: { item: IDiscipline }) => {
+    return <DisciplinesListItem discipline={item} />
+  }
+
+  useLayoutEffect(() => {
+    statusBar.setTheme('primary')
+  }, [])
+
+  useEffect(() => {
+    presenter.getDisciplines({ paginate: { page: 0, limit: 10 } })
+  }, [])
+
+  return (
+    <Container>
+      <FlatList
+        data={presenter.disciplines.results}
+        renderItem={renderDisciplinesListItem}
+      />
+
+      <Button title={'Logout'} onPress={() => auth.setToken(null)} />
+    </Container>
+  )
+}
+
+export default withDisciplinePresenter(DisciplinesScreen)
