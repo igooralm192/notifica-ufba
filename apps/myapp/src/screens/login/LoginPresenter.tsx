5e436598c8a548bbdb9a455ed6649ba0ac8a3681
Igor de Almeida <igoor1205@gmail.com>
2022-05-22 15:04:27 -0300

    feat: add myapp LoginScreen

---
+++ apps/myapp/src/screens/login/LoginPresenter.tsx
@@ -0,0 +1,60 @@
+import { IAuthenticateUserUseCase } from '@notifica-ufba/domain/usecases'
+import { BaseError } from '@notifica-ufba/errors'
+
+import * as api from '@/api'
+import { useAuth } from '@/contexts/auth'
+
+import React, { useContext, useState } from 'react'
+import Toast from 'react-native-toast-message'
+
+export interface LoginPresenterContextData {
+  loading: boolean
+  login(input: IAuthenticateUserUseCase.Input): Promise<void>
+}
+
+const LoginPresenterContext = React.createContext(
+  {} as LoginPresenterContextData,
+)
+
+export const LoginPresenter: React.FC = ({ children }) => {
+  const auth = useAuth()
+
+  const [loading, setLoading] = useState(false)
+
+  const login = async ({ email, password }: IAuthenticateUserUseCase.Input) => {
+    setLoading(true)
+
+    try {
+      const { token, user } = await api.user.login({ email, password })
+
+      auth.setToken(token)
+      auth.setUser(user)
+    } catch (err) {
+      const error = err as BaseError
+
+      Toast.show({
+        type: 'error',
+        text1: 'Erro ao fazer login.',
+        text2: error.message,
+      })
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <LoginPresenterContext.Provider value={{ loading, login }}>
+      {children}
+    </LoginPresenterContext.Provider>
+  )
+}
+
+export const withLoginPresenter = (Component: React.FC) => {
+  return (props: any) => (
+    <LoginPresenter>
+      <Component {...props} />
+    </LoginPresenter>
+  )
+}
+
+export const useLoginPresenter = () => useContext(LoginPresenterContext)
