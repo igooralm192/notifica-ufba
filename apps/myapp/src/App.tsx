347b41dc79b10e90e698888843df5ad7f7f6a132
Igor de Almeida <igoor1205@gmail.com>
2022-05-29 22:19:31 -0300

    fix: adjust App

--- apps/myapp/src/App.tsx
+++ apps/myapp/src/App.tsx
@@ -1,27 +1,9 @@
 import { AllProviders } from '@/components'
 import Routes from '@/routes'
 
-import OneSignal from 'react-native-onesignal'
-import Constants from 'expo-constants'
-import { useEffect } from 'react'
-
-OneSignal.setAppId(Constants?.manifest?.extra?.oneSignalAppId)
+import React from 'react'
 
 const App: React.FC = () => {
-  useEffect(() => {
-    OneSignal.sendTag('discipline_group_code', 't010000')
-
-    OneSignal.addSubscriptionObserver(event => {
-      console.log(event)
-    })
-
-    OneSignal.setNotificationWillShowInForegroundHandler
-
-    return () => {
-      OneSignal.clearSubscriptionObservers()
-      OneSignal.deleteTag('discipline_group_code')
-    }
-  }, [])
   return (
     <AllProviders>
       <Routes />
