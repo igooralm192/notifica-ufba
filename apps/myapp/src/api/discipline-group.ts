b2c0aa66a8991edf8eb99d4df6cbb2c7077021aa
Igor de Almeida <igoor1205@gmail.com>
2022-06-01 00:15:45 -0300

    feat(app): add getMyLastMessages endpoint

--- apps/myapp/src/api/discipline-group.ts
+++ apps/myapp/src/api/discipline-group.ts
@@ -1,5 +1,28 @@
-import { ISubscribeStudentToDisciplineGroupUseCase } from '@notifica-ufba/domain/usecases'
+import { IPaginateListInputDTO } from '@notifica-ufba/domain/dtos'
+import {
+  IReadLastMessagesUseCase,
+  ISubscribeStudentToDisciplineGroupUseCase,
+} from '@notifica-ufba/domain/usecases'
+
+import { LastMessageMapper } from '@/mappers'
 import { api } from '@/services/api'
+
+export const getMyLastMessages = async ({
+  page,
+  limit,
+}: IPaginateListInputDTO): Promise<IReadLastMessagesUseCase.Output> => {
+  const response = await api.get('/discipline-groups/last-messages', {
+    params: {
+      page,
+      limit,
+    },
+  })
+
+  return {
+    results: LastMessageMapper.toDTOList(response.data.results),
+    total: response.data.total,
+  }
+}
 
 export const subscribeStudent = async ({
   disciplineGroupId,
