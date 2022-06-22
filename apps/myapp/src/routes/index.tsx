e5f104d19d5f7c9cd08259ba1590a55292b46954
Igor de Almeida <igoor1205@gmail.com>
2022-06-22 10:56:32 -0300

    refactor: update routes

--- apps/myapp/src/routes/index.tsx
+++ apps/myapp/src/routes/index.tsx
@@ -1,8 +1,8 @@
-import { AuthState, useAuth } from '@/contexts/auth'
+import { useAuth } from '@/contexts/auth'
+import { MeProvider } from '@/contexts/me'
 import { BottomTabsNavigator } from '@/routes/bottom-tabs'
 import { AppNavigation } from '@/types/navigation'
 import {
-  DisciplineGroupMessagesScreen,
   DisciplineGroupInfoScreen,
   LoginScreen,
   RegisterScreen,
@@ -10,32 +10,20 @@ import {
   WelcomeScreen,
   DisciplineGroupTabsScreen,
 } from '@/screens'
-import { ITheme } from '@/styles/theme'
+import { AuthState } from '@/store/auth/types'
 
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import { useTheme } from '@rneui/themed'
 import React from 'react'
 
 const Stack = createStackNavigator<AppNavigation>()
 
-const getAuthScreens = (state: AuthState, theme: ITheme) => {
-  switch (state) {
-    case AuthState.UNAUTHENTICATED:
-      return (
-        <Stack.Group screenOptions={{ headerShown: false }}>
-          <Stack.Screen
-            name="WelcomeScreen"
-            component={WelcomeScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen name="LoginScreen" component={LoginScreen} />
-          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-        </Stack.Group>
-      )
-    case AuthState.AUTHENTICATED:
-      return (
-        <Stack.Group>
+const Routes: React.FC = () => {
+  const auth = useAuth()
+
+  if (auth.state === AuthState.AUTHENTICATED) {
+    return (
+      <MeProvider>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
           <Stack.Screen
             name="BottomTabsNavigator"
             component={BottomTabsNavigator}
@@ -50,40 +38,34 @@ const getAuthScreens = (state: AuthState, theme: ITheme) => {
             name="DisciplineGroupInfoScreen"
             component={DisciplineGroupInfoScreen}
           />
-        </Stack.Group>
-      )
-    default:
-      return (
+        </Stack.Navigator>
+      </MeProvider>
+    )
+  }
+
+  if (auth.state === AuthState.UNAUTHENTICATED) {
+    return (
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen
-          name="SplashScreen"
-          component={SplashScreen}
+          name="WelcomeScreen"
+          component={WelcomeScreen}
           options={{ headerShown: false }}
         />
-      )
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+      </Stack.Navigator>
+    )
   }
-}
-
-const Routes: React.FC = () => {
-  const auth = useAuth()
-  const { theme: rneTheme } = useTheme()
-
-  if (auth.loading || auth.state === AuthState.UNKNOWN) return <SplashScreen />
 
   return (
-    <NavigationContainer theme={theme}>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {getAuthScreens(auth.state, rneTheme)}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen
+        name="SplashScreen"
+        component={SplashScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
   )
-}
-
-const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    background: '#FFF',
-  },
 }
 
 export default Routes
