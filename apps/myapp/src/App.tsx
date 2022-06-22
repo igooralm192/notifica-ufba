a4096ddf44df004576e9345d816180810d2cea69
Igor de Almeida <igoor1205@gmail.com>
2022-06-22 12:22:40 -0300

    refactor: update App

--- apps/myapp/src/App.tsx
+++ apps/myapp/src/App.tsx
@@ -1,13 +1,59 @@
+import { CommonError } from '@notifica-ufba/domain/errors'
+import { BaseError } from '@notifica-ufba/errors'
+
 import { AllProviders } from '@/components'
+import env from '@/config/env'
 import Routes from '@/routes'
 
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import React from 'react'
+import Toast from 'react-native-toast-message'
+import { Options, Provider as HttpProvider } from 'use-http'
+
+const options: Partial<Options> = {
+  interceptors: {
+    request: async ({ options }) => {
+      const token = await AsyncStorage.getItem('TOKEN')
+
+      if (options.headers)
+        // @ts-ignore
+        options.headers.Authorization = token ? 'Bearer ' + token : ''
+
+      return options
+    },
+    response: async ({ response }) => {
+      if (response.status < 400) {
+        return response
+      }
+
+      if (!response.data?.code || !response?.data?.message) {
+        throw new CommonError.InternalServerError(new Error())
+      }
+
+      throw new BaseError(
+        response?.data?.code,
+        response?.data?.message,
+        response?.data?.context,
+        response?.data?.stack,
+      )
+    },
+  },
+  onError: ({ error }) => {
+    Toast.show({
+      type: 'error',
+      text1: 'Erro na requisição.',
+      text2: error.message,
+    })
+  },
+}
 
 const App: React.FC = () => {
   return (
-    <AllProviders>
-      <Routes />
-    </AllProviders>
+    <HttpProvider url={env.API_URL} options={options}>
+      <AllProviders>
+        <Routes />
+      </AllProviders>
+    </HttpProvider>
   )
 }
 
